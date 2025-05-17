39ef5d2b701e5c514241b26281c9d087ec8debff
JasonMacbook <jason.boden2072@outlook.com>
2025-05-17 16:03:23 +1000

    updated commenting

--- main.js
+++ main.js
@@ -2,13 +2,20 @@
 //create a function that has the computer randomly generate one of three strings being rock paper or scissors & stores that string in a variable
 
 function getComputersChoice(computerChoice = 3) {
-    return Math.floor(Math.random() * max) + 1;
+    return Math.floor(Math.random() * computerChoice) + 1;
 }
-let max = getComputersChoice();
+let computerChoice = getComputersChoice();
 console.log(computerChoice);
+
 //prompt the user to enter in one of the three strings and store that inside another variable
+
 function getPlayersChoice(userChoice = prompt("Choose Rock, Paper, or Scissors: ")) {
-    
+    return userChoice;
 }
-//play the round & return the result of the round, weather or not the player one or lost the round
-//track the score of the game using a human & computer score variable with the value starting at 0
+let userChoice = getPlayersChoice();
+console.log(userChoice);
+
+//declate the players score variables
+//two variables names humanScore & computerScore, initialize the variables with the value of 0
+//write the logic to play a single round, create a function called playRound
+//write logic to play the entire game
