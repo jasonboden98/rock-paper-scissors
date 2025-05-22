ff42b3775dc7ec3557d0ad0cfe71e2252946a19c
JasonMacbook <jason.boden2072@outlook.com>
2025-05-22 13:33:58 +1000

    cleaning up code

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 //Making a Rock Paper scissors game using javascript & outputting to the console
 //create a function that has the computer randomly generate one of three strings being rock paper or scissors & stores that string in a variable
-
 function getComputersChoice(computerChoice = 3) {
     return Math.floor(Math.random() * computerChoice) + 1;
 }
 let computerChoice = getComputersChoice();
 // console.log(computerChoice);
 
-// TODO -- fix variable declared but never used --FIXED
 let result = " ";
 if (computerChoice == 1) {
     result = "Rock"
@@ -18,15 +16,17 @@ else if (computerChoice == 2) {
 else if (computerChoice == 3) {
     result = "Scissors"
 }
-console.log(result);
+// console.log("The computer chose " + result);
 
 //prompt the user to enter in one of the three strings and store that inside another variable
-
 function getPlayersChoice(userChoice = prompt("Choose Rock, Paper, or Scissors: ")) {
     return userChoice;
 }
 let userChoice = getPlayersChoice();
-console.log(userChoice);
+// console.log("You chose " + userChoice);
+
+
+
 
 //declate the players score variables
 //two variables names humanScore & computerScore, initialize the variables with the value of 0
